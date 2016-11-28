3fa13bef69af79d74845094934ee32d3359b5473
Mario de Biase <debiasej@gmail.com>
2016-11-28 16:21:16 +0100

    Add a handler so as to control the password

--- src/app/BankCardContainer.js
+++ src/app/BankCardContainer.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Card, CardActions, CardTitle} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 import CardSelector from './CardSelector.js';
+import ConfirmOperationTextField from './ConfirmOperationTextField.js';
 
-const BankCardContainer = () => (
-  <Card>
-    <CardTitle title="Select a card" />
-    <CardActions>
-      <CardSelector />
-      <FlatButton label="Continue" />
-    </CardActions>
-  </Card>
-);
+
+class BankCardContainer extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      showPasswordField: false,
+    };
+
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+  }
+
+  handleTouchTap() {
+    this.setState({
+      showPasswordField: !this.state.showPasswordField
+    });
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardTitle title="Select a card" />
+        <CardActions>
+          <CardSelector />
+          {this.state.showPasswordField ?  <ConfirmOperationTextField /> : null}
+          <br />
+          <FlatButton label="Continue" primary={true} onTouchTap={this.handleTouchTap} />
+        </CardActions>
+      </Card>
+    );
+  }
+}
 
 export default BankCardContainer;
---
+++ src/app/ConfirmOperationTextField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+const TextFieldExampleSimple = () => (
+  <div>
+    <TextField
+      hintText="Password Field"
+      floatingLabelText="Password"
+      type="password"
+    />
+  </div>
+);
+
+export default TextFieldExampleSimple;
