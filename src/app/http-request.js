3de6cc7dabb83793432ab2ab251308038cbede24
Mario de Biase <debiasej@gmail.com>
2016-11-29 08:44:21 +0100

    Add http-request package

---
+++ src/app/http-request.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import Request from 'react-http-request';
+
+class HTTPRequest extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    
+    return (
+      <Request
+        url={this.props.url}
+        method='get'
+        accept='application/json'
+        verbose={true}
+      >
+        {
+          ({error, result, loading}) => {
+            if (loading) {
+              return <div>loading...</div>;
+            } else {
+              return <div><pre>{JSON.stringify(result, null, 2) }</pre></div>;;
+            }
+          }
+        }
+      </Request>
+    );
+  }
+}
+
+export default HTTPRequest;
